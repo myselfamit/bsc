5063424701a83714c33fd76253407abdd5ddd11a
Amit_Chaudhari <amitchaudhari333@gmail.com>
2025-06-08 14:58:43 -0400

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  publicDir: 'public',
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          // Keep video files in root of dist
+          if (assetInfo.name && assetInfo.name.endsWith('.mp4')) {
+            return '[name][extname]'
+          }
+          return 'assets/[name]-[hash][extname]'
+        }
+      }
+    }
+  }
 })
