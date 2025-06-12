07bd7ea46465cdd4544f0affa3569f1b15c88779
Amit_Chaudhari <amitchaudhari333@gmail.com>
2025-06-11 20:25:20 -0400

    vedio path

--- src/HeroSection.jsx
+++ src/HeroSection.jsx
@@ -51,6 +51,7 @@ const HeroSection = ({ scrollY, handleActionButtonClick }) => {
           animate={{ opacity: isInView ? getVideoOpacity() : 0 }}
           transition={{ duration: 0.8, ease: "easeOut" }}
         >
+          {/* <source src="https://bombay-social-club-assets.s3.ca-central-1.amazonaws.com/bsc.mp4" type="video/mp4" /> */}
           <source src="/bsc.mp4" type="video/mp4" />
           <source src="/bsc.webm" type="video/webm" />
           Your browser does not support the video tag.
